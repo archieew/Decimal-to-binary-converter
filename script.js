32322e9f21ebb04218cc9070f854e189ccbcac2f
Archie Custodio <archie06custodio@gmail.com>
2025-08-27 22:46:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const checkUserInput = () => {
-  console.log(numberInput.value)
+  console.log(numberInput.value);
 };
+
+convertBtn.addEventListener("click", checkUserInput)

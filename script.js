fd63a538a98f113911f8ab60a4b3a02fbb962769
Archie Custodio <archie06custodio@gmail.com>
2025-09-11 20:55:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ const animationData = [
     inputVal: 5,
     addElDelay: 1000
   },
-   {
+  {
     inputVal: 2,
     addElDelay: 1500
   },
-  {
+    {
     inputVal: 1,
     addElDelay: 2000
-  }
+  },
+  
 ];
 
 const decimalToBinary = (input) => {

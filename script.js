d1d7434381af157d6aa361168b4f55dbd2087b95
Archie Custodio <archie06custodio@gmail.com>
2025-09-11 21:03:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,9 @@ const showAnimation = () => {
 
   animationData.forEach((obj) => {
     setTimeout(() => {
-      animationContainer.innerHTML += `<p id = ""></p>`;
+      animationContainer.innerHTML += `
+        <p id="${obj.inputVal}"></p>
+      `;
     }, obj.addElDelay);
   });
 };

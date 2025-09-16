4efc232a700384746dcbebf7427b2601367eb12a
Archie Custodio <archie06custodio@gmail.com>
2025-09-17 02:10:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -50,9 +50,9 @@ const showAnimation = () => {
       document.getElementById(obj.inputVal).textContent = obj.msg;
     }, obj.showMsgDelay);
 
-setTimeout(() => {
-
-}, obj.removeElDelay)
+    setTimeout(() => {
+document.getElementById(obj.inputVal).remove()
+    }, obj.removeElDelay);
   });
 };
 
